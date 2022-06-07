13d6b897fa2de765edcb222fee0b07058972dbb8
kmaalig <106278777+kmaalig@users.noreply.github.com>
2022-06-07 09:48:38 -0500

    Updated For Loops JS

--- js/for-loops.js
+++ js/for-loops.js
@@ -1,4 +1,21 @@
 (function() {
 	"use strict";
 
+	//Example:
+	//	for (var i = 0; i < 10; i++) {
+	//     console.log('for loop iteration #' + i);
+	// 	}
+
+	function showMultiplicationTable(num) {
+
+		// let num =(prompt('Enter a number:'))
+
+		for (let i = 0; i <= 10; i++){
+			let result = i * num;
+			console.log(num + ' x ' + i + ' = ' + result )
+		}
+	}
+
+	showMultiplicationTable(3);
+
 })();
