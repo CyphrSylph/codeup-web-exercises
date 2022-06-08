7218502f6db5df55cdcb6902266e55ab2ac3357a
kmaalig <106278777+kmaalig@users.noreply.github.com>
2022-06-08 10:51:37 -0500

    Break & Continue JS Completed

--- js/break-and-continue.js
+++ js/break-and-continue.js
@@ -9,37 +9,53 @@
 	//Use a loop and the continue statement to output all the odd number
 	//between 1 and 50, except for the number the user entered.
 
-	function oddNum() {
+	let userNum = (prompt('Enter an odd number (1-49):'))
 
-		while (true) {
-
-			var userNum = (prompt('Enter an odd number (1-49):'))
-
-			if (userNum % 2 === 0) {
-				console.log(userNum + ' is not an odd number.');
-			} else if (userNum < 1 || userNum > 50) {
-				console.log(userNum + 'is not between 1-50.');
-			} else {
-				break;
-			}
+	while (true) {
+		if (userNum % 2 === 0) {
+			console.log(userNum + ' is not an odd number.');
+		} else if (userNum < 1 || userNum > 50) {
+			console.log(userNum + 'is not between 1-50.');
+		break;
 		}
-			console.log(userNum + ' is the number to skip!');
+	}
+		console.log(userNum + ' is the number to skip!');
 
+	userNum = Number(userNum) // Converts userNum to Number data type and avoids type coercion
 
-			for (let i = 0; i <= 50; i++) {
-				if (i % 2 !== 0) {
-					if(userNum == i){
-						console.log("yikes skipping number " + i);
-						continue;
-					}
-					console.log(i + ' is an odd number');
-
-				}
-
-
+	for (let i = 0; i <= 50; i++) {
+		if (i % 2 !== 0) {
+			if(userNum === i){
+				console.log("Yikes! Skipping number " + i);
+				continue;
 			}
+			console.log(i + ' is an odd number');
+
+		}
 
 	}
-	oddNum();
+
+
+	// Alternative Option:
+	let userNum1 = prompt("Hello user - can you give me an odd number between 1 and 50?");
+
+	while(true){
+		if(userNum1 % 2 !== 0 && userNum1 >= 1 && userNum1 >= 50) {
+			console.log('Odd number detected- Access granted');
+			break;
+		}
+		userNum1 = prompt('That is not an odd number- try again')
+	}
+
+	userNum1 = Number(userNum1) // Converts userNum to Number data type and avoids type coercion
+
+	for(let i = 1; i <= 50; i++){
+		if(i % 2 !== 0){
+			continue;
+		} else if (i === userNum1){
+			console.log('Eeep! Skipping' + userNum1);
+		}
+		console.log(i);
+	}
 
 })();
