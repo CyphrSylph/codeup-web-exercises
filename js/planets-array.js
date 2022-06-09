a3ab648c6e56e5261ef4982a13455f40b5178c46
kmaalig <106278777+kmaalig@users.noreply.github.com>
2022-06-09 15:21:12 -0500

    Planets Array JS Updated

--- js/planets-array.js
+++ js/planets-array.js
@@ -18,19 +18,20 @@
 	 * the step that it describes
 	 */
 
-	planets.unshift('Sun');
+
 	console.log('Adding "The Sun" to the beginning of the planets array.');
+	planets.unshift('Sun');
 	console.log(planets);
 
-	planets.push('Pluto')
 	console.log('Adding "Pluto" to the end of the planets array.');
+	planets.push('Pluto');
 	console.log(planets);
 
-	let removeSun = planets.shift()
+	let removeSun = planets.shift();
 	console.log('Removing "The Sun" from the beginning of the planets array.' + removeSun);
 	console.log(planets);
 
-	let removePluto = planets.pop()
+	let removePluto = planets.pop();
 	console.log('Removing "Pluto" from the end of the planets array.' + removePluto);
 	console.log(planets);
 
