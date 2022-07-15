d6fd0dbd86b259fa0823af71c5235d97acb7f590
kmaalig <106278777+kmaalig@users.noreply.github.com>
2022-07-15 13:47:26 -0500

    Updated Mapbox API token

--- js/keys.js
+++ js/keys.js
@@ -1,4 +1,4 @@
 // API KEYS
 
 // MAPBOX API
-const mapboxAPIkey = 'pk.eyJ1IjoiY29keWg0MiIsImEiOiJjbDVsNDl4djUwMGZlM2xvZm1ldDhvajZpIn0.B4k7KNivHHoVU5Bm_uO24w\n';
+const mapboxAPIkey = 'pk.eyJ1Ijoia21hYWxpZyIsImEiOiJjbDVtb282dXAwdzA4M2NsdjZobGxxYXlvIn0.yxLjHPoWkHRM4A5Hkcv8ww';
