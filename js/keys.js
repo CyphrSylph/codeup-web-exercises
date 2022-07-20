020ee469eb6a0bbd9ef373c48e36fdd807aba265
kmaalig <106278777+kmaalig@users.noreply.github.com>
2022-07-20 15:14:11 -0500

    Added GitHub token to keys

--- js/keys.js
+++ js/keys.js
@@ -2,3 +2,4 @@
 
 // MAPBOX API
 const mapboxAPIkey = 'pk.eyJ1Ijoia21hYWxpZyIsImEiOiJjbDVtb282dXAwdzA4M2NsdjZobGxxYXlvIn0.yxLjHPoWkHRM4A5Hkcv8ww';
+const gitHubAPIkey = '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';
