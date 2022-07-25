b2aea0e806d74665b13a1c6741983ce8fba8e446
kmaalig <106278777+kmaalig@users.noreply.github.com>
2022-07-25 15:20:54 -0500

    Added feature to submit new quote

--- jalopy-main/js/views/QuotesAPI.js
+++ jalopy-main/js/views/QuotesAPI.js
@@ -1,3 +1,5 @@
+import createView from "../createView.js";
+
 // FUNCTION TO SET VIEW OF QUOTES FROM API
 export default function quotesAPIView(props) {
 	let html = `
@@ -7,7 +9,12 @@ export default function quotesAPIView(props) {
 		</header>
 		<main>
 			<div id="my-quotesAPI">
-			${addQuotes(props.quotes)}
+				${addQuotes(props.quotes)}
+				<div id="new-quote-div">
+					<input placeholder="Enter new quote here..." id="newQuoteText">
+<!--					<input placeholder="Add the author here..." id="newAuthorText">-->
+					<button id="addBtn" type="button" value="+">+</button>
+				</div>
 			</div>
 		</main>
 	`;
@@ -29,4 +36,46 @@ function addQuotes(quotes) {
 	return html;
 }
 
+// FUNCTION TO ADD A NEW QUOTE FROM TEXT INPUT
+export function addNewQuoteEvent() {
+const addBtn = document.querySelector('#addBtn');
+addBtn.addEventListener('click', addNewQuote);
+}
+
+function addNewQuote() {
+	// USE .trim() TO TRIM WHITESPACE FROM A TEXT INPUT WHEN VALIDATING
+	const quoteInput = document.querySelector('#newQuoteText');
+	const quote = quoteInput.value.trim();
+	// const authorInput = document.querySelector('#newAuthorText');
+	// const author = authorInput.value.trim();
+	if (quote.length < 1) {
+		// ADD ALERT OF SHAKE FUNCTION HERE FOR VALIDATION
+		quoteInput.classList.add('animate__animated', 'animate__shakeX', 'inputError');
+		console.log('error');
+		return;
+	}
+	// if (author.length > 1) {
+	// 	authorInput.value = 'Anonymous';
+	// 	console.log('error');
+	// 	return;
+	// }
+	const requestOpts = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': QUOTES_API_KEY
+		},
+		// CONVERT OBJECT INTO STRING ELEMENT FOR FETCH REQUEST
+		body: JSON.stringify([{quote}])
+	}
+	fetch('https://quotes.fulgentcorp.com:12250/v1/quotes?random=true&limit=5', requestOpts)
+		.then(function (response) {
+			if (!response.ok) {
+				// LOG ERROR FOR QUOTE
+			} else {
+				// ADD QUOTE & REDIRECT WITHIN VIEW
+				createView('/quotesAPI');
+			}
+		});
+}
 
